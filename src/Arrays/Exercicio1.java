0fc3d50076551e361f88f97f99e5705a86fb9920
StJ0hn <jonhmiguel138@gmail.com>
2026-01-22 21:31:27 -0300

    Exercicio 1 de Array concluído

---
+++ src/Arrays/Exercicio1.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("How many numbers you type? ");
+        int n = sc.nextByte();
+        int[] vect = new int[n];
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            System.out.print("Type a number: ");
+            vect[i] = sc.nextByte();;
+        }
+        System.out.println("NEGATIVE NUMBERS");
+        for (int i = 0; i < n; i++) {
+            if (vect[i] < 0){
+                System.out.printf("%d%n", vect[i]);
+            }
+        }
+    }
+}
